b00db90e332a932e45591ec28227c48f8906cb19
gthaberland <gustavo.tudela@ctfc.cat>
2025-04-30 12:51:57 +0200

    Clima data input

--- test_01.R
+++ test_01.R
@@ -8,13 +8,43 @@ InitialCPool_load = c(0,0,0,0,0)
 # simulation runs for long time to understand how long it takes to go to zero
 SimulationTime_load = seq(1:200)
 
+# Input Debris DB
 input_data = readRDS("../../../2024/03_Forest_debris_decomposition/01_Std_Restes_CPF/DB_processades_rds/DB_debris_species.rds")
 input_data$Id_Inventari = as.factor(input_data$Id_Inventari)
 input_data$Species = as.factor(input_data$Species)
 
 plots_ID = levels(input_data$Id_Inventari)
 
-# draft of a nested loop
+# Input climatic data
+library(readxl) #importar de excel
+library(lubridate) #extract the year and month of a date
+library(dplyr)
+
+input_clima = read_excel("../../../2024/Meteo/Historical_meteo/Historical_meteo.xlsx")
+str(input_clima)
+input_clima$Year <- year(input_clima$dates)
+input_clima$Month <- month(input_clima$dates)
+names(input_clima)
+input_clima <- input_clima %>%
+      select(2, 8, 12, 23)
+
+na_ids_year_93 <- input_clima %>%
+      filter(is.na(MeanTemperature), Year > 1993) %>%
+      select(Id_Inventari) %>%
+      distinct()
+
+Clima <- input_clima %>%
+      select(Id_Inventari, MeanTemperature, Year) %>%
+      group_by(Id_Inventari, Year) %>%
+      summarise(
+            MeanTemp = round(mean(MeanTemperature, na.rm = TRUE), 2),
+            N = sum(!is.na(MeanTemperature)),  # or N = n() if no NAs
+            .groups = "drop"
+      )
+
+
+
+      # draft of a nested loop
 # #outer loop by plot id
 # for(i in 1:length(plots_ID)){
 #       selected_plot = plots_ID[i]
